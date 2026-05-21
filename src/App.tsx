d9890b46c769f3920114c068ae66b2656ca2f455
Sarathi2511 <shreesarathielectricalsonline@gmail.com>
2026-05-21 17:58:50 +0530

    Updated Privacy and T&C Page !

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import type { ReactElement } from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { LandingPage } from '@/modules/landing/LandingPage'
+import { PrivacyPolicyPage } from '@/modules/landing/PrivacyPolicyPage'
+import { TermsConditionsPage } from '@/modules/landing/TermsConditionsPage'
 
 export default function App(): ReactElement {
   return (
     <BrowserRouter>
-      <LandingPage />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+        <Route path="/terms-conditions" element={<TermsConditionsPage />} />
+      </Routes>
     </BrowserRouter>
   )
 }
